5e68d5d62d8d71493fd5f0ee73bec7a26eb86f90
Mũrĩthi Borona <borona.murithi@gmail.com>
2015-11-27 14:36:05 +0300

    Create withdrawal account test.

--- test/test_accounts.rb
+++ test/test_accounts.rb
@@ -16,6 +16,22 @@ class AccountsTest < Test::Unit::TestCase
     assert_equal(STATUS_SUCCESS, response.status)
   end
 
+  def test_create_withdrawal_account
+    omit_if(TEST_TX_ACCOUNT_NAME.empty?)
+    response = @lipisha.create_withdrawal_account(1,
+                                                  TEST_TX_ACCOUNT_NAME,
+                                                  TEST_TX_ACCOUNT_NUMBER,
+                                                  TEST_TX_ACCOUNT_BANK_NAME,
+                                                  TEST_TX_ACCOUNT_BANK_BRANCH,
+                                                  TEST_TX_ACCOUNT_BANK_ADDRESS,
+                                                  TEST_TX_ACCOUNT_SWIFT_CODE,
+                                                  TEST_TX_ACCOUNT_MANAGER)
+    puts(response)
+    assert_not_nil response
+    assert_equal(STATUS_SUCCESS, response.status)
+    assert_equal(TEST_TX_ACCOUNT_NUMBER, response.content["transaction_account_number"])
+  end
+
   def test_create_user
     omit_if(TEST_USER_ROLE.empty?)
     response = @lipisha.create_user(TEST_USER_NAMES,
