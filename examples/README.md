5bd6cf000073a8c61f6e0e1d941e87c1bb650255
Mũrĩthi Borona <borona.murithi@gmail.com>
2015-11-28 16:58:53 +0300

    Adding IPN Example (Rails)

--- examples/README.md
+++ examples/README.md
@@ -6,3 +6,28 @@ This is an examples of Lipisha IPN integration for rails.
 This assumes that you have configured an IPN URL for API callbacks.
 
 IPN: Instant Payment Notification
+
+## Usage
+
+The bulk of the logic happens in the IPN controller.
+
+    https://github.com/lipisha/lipisha-ruby-sdk/tree/master/examples/app/controllers/ipn_controller.rb
+
+For production usage, handling IPN callbacks should be mapped to records in permanent storage.
+
+Adjust the controller settings to load your *API_KEY* and *API_SIGNATURE*.
+
+
+## Running
+
+Make sure you have *Ruby* and the *bundle* gem installed
+
+```shell
+gem install bundle
+bundle install
+./bin/rails server
+```
+
+Test HTTP requests may then be POSTED test parameters to
+
+    http://localhost:3000/IPN/
---
+++ examples/app/controllers/ipn_controller.rb
@@ -0,0 +1,55 @@
+class IpnController < ApplicationController
+
+  API_KEY = ENV["LIPISHA_API_KEY"]
+  API_SIGNATURE = ENV["LIPISHA_API_SIGNATURE"]
+  API_VERSION = "1.3.0"
+  ACTION_INITIATE = "Initiate"
+  ACTION_ACKNOWLEDGE = "Acknowledge"
+  ACTION_RECEIPT = "Receipt"
+  STATUS_SUCCESS = "Success"
+  STATUS_SUCCESS_CODE = "001"
+
+  def post
+    if ((params["api_key"]==API_KEY and params["api_signature"]==API_SIGNATURE))
+      api_type = params["api_type"]
+      Rails.logger.debug("Request data #{params}")
+      if (api_type==ACTION_INITIATE)
+        # Acknowledge API CALL
+        #
+        # Perform processing and storage here if required
+
+        # Respond to Lipisha to confirm transaction has been received.
+        # Lipisha would then acknowledge this request - see below
+        response = {
+          "api_key" => API_KEY,
+          "api_signature" => API_SIGNATURE,
+          "api_version" => API_VERSION,
+          "api_type" => ACTION_RECEIPT,
+          "transaction_reference" => params["transaction_reference"],
+          "transaction_status_code" => STATUS_SUCCESS_CODE,
+          "transaction_status" => STATUS_SUCCESS,
+          "transaction_status_description" => "Transaction received",
+          "transaction_custom_sms" => "Payment received. Thank you."
+        }
+        render :json => response
+      elsif (api_type=ACTION_ACKNOWLEDGE)
+        # Lipisha acknowledges the transaction
+        # At this point we can update our records confirming that the cash has actually been received.
+        
+        transaction_reference = params["transaction_reference"]
+        transaction_status = params["transaction_status"]
+        transaction_status_code = params["transaction_status_code"]
+        transaction_status_description = params["transaction_status_description"]
+        Rails.logger.debug("Received acknowledgemnt: TX: #{transaction_reference} | Status: #{transaction_status}")
+        render :text => "OK"
+      else
+        render :text => "Invalid Request", :status => :bad_request
+      end
+    else
+      Rails.logger.error("Invalid IPN Credentials")
+      render :text => "Invalid Credentials", :status => :unauthorized
+    end
+  end
+
+  skip_before_filter :verify_authenticity_token
+end
